af748762d8e79affa206cdd0f6525257694d4f1a
Eitan Talmi <71331120+eitanam@users.noreply.github.com>
2023-12-11 13:50:46 +0200

    Add files via upload

---
+++ MFAStatusReport.ps1
@@ -0,0 +1,47 @@
+﻿Connect-MsolService
+Write-Host "Finding Azure Active Directory Accounts..."
+$Users = Get-MsolUser -All | Where-Object { $_.UserType -ne "Guest" }
+$Report = [System.Collections.Generic.List[Object]]::new()
+Write-Host "Processing" $Users.Count "accounts..." 
+ForEach ($User in $Users) {
+	$MFADefaultMethod = ($User.StrongAuthenticationMethods | Where-Object { $_.IsDefault -eq "True" }).MethodType
+    $MFAPhoneNumber = $User.StrongAuthenticationUserDetails.PhoneNumber
+    $PrimarySMTP = $User.ProxyAddresses | Where-Object { $_ -clike "SMTP*" } | ForEach-Object { $_ -replace "SMTP:", "" }
+    $Aliases = $User.ProxyAddresses | Where-Object { $_ -clike "smtp*" } | ForEach-Object { $_ -replace "smtp:", "" }
+
+    If ($User.StrongAuthenticationRequirements) {
+        $MFAState = $User.StrongAuthenticationRequirements.State
+    }
+    Else {
+        $MFAState = 'Disabled'
+    }
+
+    If ($MFADefaultMethod) {
+        Switch ($MFADefaultMethod) {
+            "OneWaySMS" { $MFADefaultMethod = "SMS" }
+            "TwoWayVoiceMobile" { $MFADefaultMethod = "Mobile Phone" }
+            "TwoWayVoiceOffice" { $MFADefaultMethod = "Office Phone" }
+            "PhoneAppOTP" { $MFADefaultMethod = "Auth app or HW token" }
+            "PhoneAppNotification" { $MFADefaultMethod = "Microsoft auth app" }
+        }
+    }
+    Else {
+        $MFADefaultMethod = "Disabled"
+    }
+  
+    $ReportLine = [PSCustomObject] @{
+        UserPrincipalName = $User.UserPrincipalName
+        DisplayName       = $User.DisplayName
+        MFAState          = $MFAState
+        MFADefaultMethod  = $MFADefaultMethod
+        MFAPhoneNumber    = $MFAPhoneNumber
+        PrimarySMTP       = ($PrimarySMTP -join ',')
+        Aliases           = ($Aliases -join ',')
+    }
+                 
+    $Report.Add($ReportLine)
+}
+
+Write-Host "Report is in c:\temp\MFAUsers.csv"
+
+$Report | Sort-Object UserPrincipalName | Export-CSV -Encoding UTF8 -NoTypeInformation c:\temp\MFAUsers.csv
